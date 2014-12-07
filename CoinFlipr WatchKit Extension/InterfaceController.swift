bf7eb1ec1c7d464b135cd1ee1267a0f4a5f25dc9
benbueltmann <benbueltmann@gmail.com>
2014-12-06 19:00:50 -0600

    flip coins

---
+++ CoinFlipr WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,73 @@
+//
+//  InterfaceController.swift
+//  CoinFlipr WatchKit Extension
+//
+//  Created by Ben Bueltmann on 12/6/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+
+    @IBOutlet weak var headsButton: WKInterfaceButton!
+    @IBOutlet weak var tailsButton: WKInterfaceButton!
+    @IBOutlet weak var flipGroup: WKInterfaceGroup!
+    @IBOutlet weak var coinImage: WKInterfaceImage!
+    @IBOutlet weak var headsGroup: WKInterfaceGroup!
+    @IBOutlet weak var resultsGroup: WKInterfaceGroup!
+    @IBOutlet weak var resultsLabel: WKInterfaceLabel!
+    @IBAction func onFlip() {
+
+        coinImage.setImageNamed("flip")
+        coinImage.startAnimatingWithImagesInRange(NSMakeRange(1, 30), duration: 0.7, repeatCount: 0)
+        flipGroup.setHidden(true)
+        headsGroup.setHidden(false)
+    }
+
+    @IBAction func onHeads() {
+        coinImage.setImageNamed("tails")
+        resultsGroup.setHidden(false)
+        resultsLabel.setTextColor(UIColor.redColor())
+        resultsLabel.setText("Wrong, it was tails!")
+        headsGroup.setHidden(true)
+
+    }
+
+    @IBAction func onTails() {
+        coinImage.setImageNamed("heads")
+        resultsGroup.setHidden(false)
+        resultsLabel.setTextColor(UIColor.redColor())
+        resultsLabel.setText("Wrong, it was heads!")
+        headsGroup.setHidden(true)
+
+    }
+
+    override init(context: AnyObject?) {
+        // Initialize variables here.
+        super.init(context: context)
+        headsGroup.setHeight(0)
+        coinImage.setImageNamed("heads")
+        headsGroup.setHidden(true)
+        resultsGroup.setHidden(true)
+        // Configure interface objects here.
+        NSLog("%@ init", self)
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        NSLog("%@ will activate", self)
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        NSLog("%@ did deactivate", self)
+        super.didDeactivate()
+    }
+
+
+}
